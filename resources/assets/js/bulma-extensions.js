fb86f4234e21ac4df7394fc16cdadb1d406353d0
Luswata <samuel.1.luswata@nokia.com>
2018-03-19 14:34:06 +0300

    Adding all project files

---
+++ resources/assets/js/bulma-extensions.js
@@ -0,0 +1,9 @@
+
+require('bulma-extensions/bulma-accordion/dist/bulma-accordion');
+require('bulma-extensions/bulma-calendar/dist/bulma-calendar');
+require('bulma-extensions/bulma-carousel/dist/bulma-carousel');
+require('bulma-extensions/bulma-iconpicker/dist/bulma-iconPicker');
+require('bulma-extensions/bulma-quickview/dist/bulma-quickview');
+require('bulma-extensions/bulma-slider/dist/bulma-slider');
+require('bulma-extensions/bulma-steps/dist/bulma-steps');
+require('bulma-extensions/bulma-tagsinput/dist/bulma-tagsinput');
